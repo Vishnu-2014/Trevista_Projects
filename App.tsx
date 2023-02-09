77be5a8dfc84349cfca071ac898de65bc0360a5f
Vishnu-2014 <vishnureddy2014@gmail.com>
2023-02-09 16:03:19 +0530

    DoctorLoginEmail
    Created a file as DoctorLoginEmail

--- App.tsx
+++ App.tsx
@@ -6,10 +6,11 @@ import Onboarding_one from './src/components/Onboarding_one';
 import Onboarding_two from './src/components/Onboarding_two';
 import Onboarding_three from './src/components/Onboarding_three';
 import LoginEmail from './src/components/LoginEmail';
+import DoctorLoginEmail from './src/components/Doctor/DoctorLoginEmail';
 
 const App = () => {
   return (
-    <LoginEmail/>
+    <DoctorLoginEmail/>
   );
 }
 
